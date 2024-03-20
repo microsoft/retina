869b3ee6b2ee8d07f3bb2afcc9b5316dc6358b78
Paul Yu <paul.d.yu@gmail.com>
2024-03-20 11:47:25 -0700

    feat: adding additional tooling

--- .devcontainer/installMoreTools.sh
+++ .devcontainer/installMoreTools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the required tools and dependencies
-sudo apt-get update && sudo apt-get install lsb-release wget software-properties-common gnupg -y
+sudo apt-get update && sudo apt-get install -y lsb-release wget software-properties-common gnupg clang-14 lldb-14 lld-14 clangd-14 man-db
 
 # Install LLVM 14
 export LLVM_VERSION=14
