b3b8d74ba87de9d67bb29dc3474c2be0b3f6b229
Evan Baker <rbtr@users.noreply.github.com>
2024-04-15 12:29:40 -0500

    fix: cli command structure (#263)
    # Description
    
    Changes the CLI command structure from Command method "constructors" to
    direct struct initialization of package variables and flag/registration
    init hooks.
     
    This seems like a trivial syntactic difference, but due to the way that
    Cobra initializes the CLI runtime (including things like binding flags),
    it is critical that all Commands are already instantiated, and all flags
    registered, by the time the CLI framework starts running.
    
    In the previous pattern, for example, the flag for `--kubeconfig` was
    bound in the constructor for the nested `capture create` Command and,
    although Cobra detected and parsed the flag value correctly at runtime,
    it was not _available_ inside that nested command execution and
    evaluated to the zero-value.
    
    Additionally, this is the Command/init pattern demonstrated in the
    [Cobra
    docs](https://github.com/spf13/cobra/blob/main/site/content/user_guide.md#create-rootcmd),
    and we should stay aligned with their documented best-practices.
    
    Finally I think it's much clearer the previous pattern, but I am used to
    reading and writing Cobra-style CLIs 🙂
    
    ## Related Issue
    
    I started investigating #258 and ended up here. Incidentally fixes #258.
    
    ## Checklist
    
    - [x] I have read the [contributing
    documentation](https://retina.sh/docs/contributing).
    - [x] I signed and signed-off the commits (`git commit -S -s ...`). See
    [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [x] I have correctly attributed the author(s) of the code.
    - [x] I have tested the changes locally.
    - [x] I have followed the project's style guidelines.
    - [x] I have updated the documentation, if necessary.
    - [x] I have added tests, if applicable.
    
    ## Screenshots (if applicable) or Testing Completed
    
    Please add any relevant screenshots or GIFs to showcase the changes
    made.
    
    ## Additional Notes
    
    Add any additional notes or context about the pull request here.
    
    ---
    
    Please refer to the [CONTRIBUTING.md](../CONTRIBUTING.md) file for more
    information on how to contribute to this project.
    
    ---------
    
    Signed-off-by: Evan Baker <rbtr@users.noreply.github.com>

--- cli/cmd/trace.go
+++ cli/cmd/trace.go
@@ -3,28 +3,27 @@
 package cmd
 
 import (
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
-func TraceCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "trace",
-		Short: "retrieve status or results from Retina",
-	}
-	cmd.AddCommand(getTrace())
-	return cmd
+var trace = &cobra.Command{
+	Use:   "trace",
+	Short: "retrieve status or results from Retina",
 }
 
-func getTrace() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "get",
-		Short: "Retrieve network trace results with operation ID",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			operationID, _ := cmd.Flags().GetString("operationID")
-			return RetinaClient.GetTrace(operationID)
-		},
-	}
-	cmd.Flags().String("operationID", "", "Network Trace Operation ID")
-	_ = cmd.MarkFlagRequired("operationID")
-	return cmd
+var getTrace = &cobra.Command{
+	Use:   "get",
+	Short: "Retrieve network trace results with operation ID",
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		operationID, _ := cmd.Flags().GetString("operationID")
+		return errors.Wrap(RetinaClient.GetTrace(operationID), "failed to get traces")
+	},
+}
+
+func init() {
+	getTrace.Flags().String("operationID", "", "Network Trace Operation ID")
+	_ = getTrace.MarkFlagRequired("operationID")
+	trace.AddCommand(getTrace)
+	Retina.AddCommand(trace)
 }
