3ade71d8fcb4111abecd94ffb668cefd9762744d
parkjeongryul <vita400@kakao.com>
2024-04-26 01:37:19 +0900

    Apply feedback: wrap error

--- cli/cmd/capture/create.go
+++ cli/cmd/capture/create.go
@@ -314,7 +314,7 @@ func createCaptureF(kubeClient kubernetes.Interface) (*retinav1alpha1.Capture, e
 	if s3Bucket != "" {
 		secretName, err := createSecretFromS3Upload(kubeClient, s3AccessKeyID, s3SecretAccessKey, name)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to create s3 upload secret: %w")
 		}
 		capture.Spec.OutputConfiguration.S3Upload = &retinav1alpha1.S3Upload{
 			Endpoint:   s3Endpoint,
