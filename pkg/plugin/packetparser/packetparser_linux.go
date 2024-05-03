f625f94c46dcb998867bf4d063356c18aaa1314c
Evan Baker <rbtr@users.noreply.github.com>
2024-05-03 18:12:42 +0000

    feat: migrate images to mariner
    Signed-off-by: Evan Baker <rbtr@users.noreply.github.com>

--- pkg/plugin/packetparser/packetparser_linux.go
+++ pkg/plugin/packetparser/packetparser_linux.go
@@ -42,11 +42,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go@master -cc clang-14 -cflags "-g -O2 -Wall -D__TARGET_ARCH_${GOARCH} -Wall" -target ${GOARCH} -type packet packetparser ./_cprog/packetparser.c -- -I../lib/_${GOARCH} -I../lib/common/libbpf/_src -I../filter/_cprog/
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go@master -cflags "-g -O2 -Wall -D__TARGET_ARCH_${GOARCH} -Wall" -target ${GOARCH} -type packet packetparser ./_cprog/packetparser.c -- -I../lib/_${GOARCH} -I../lib/common/libbpf/_src -I../filter/_cprog/
 
-var (
-	errNoOutgoingLinks = errors.New("could not determine any outgoing links")
-)
+var errNoOutgoingLinks = errors.New("could not determine any outgoing links")
 
 // New creates a packetparser plugin.
 func New(cfg *kcfg.Config) api.Plugin {
