fb3c073c99c6956d2f77c224b56fe035ce510041
Quang Nguyen <nguyenquang@microsoft.com>
2024-05-10 15:20:00 -0400

    working e2e for advance dns

--- test/e2e/framework/kubernetes/upgrade-retina-helm.go
+++ test/e2e/framework/kubernetes/upgrade-retina-helm.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/cli"
 	helmValues "helm.sh/helm/v3/pkg/cli/values"
 	"helm.sh/helm/v3/pkg/getter"
@@ -41,10 +40,18 @@ func (u *UpgradeRetinaHelmChart) Run() error {
 	client.WaitForJobs = true
 	client.Timeout = upgradeTimeout
 
-	chart, err := loader.Load(u.ChartPath)
+	// Create a new Get action
+	get := action.NewGet(actionConfig)
+
+	// Get the current release
+	rel, err := get.Run(u.ReleaseName)
 	if err != nil {
-		return fmt.Errorf("failed to load chart: %w", err)
+		return fmt.Errorf("failed to get release: %w", err)
 	}
+
+	// Get the chart from the current release
+	chart := rel.Chart
+
 	// enable advanced metrics profile
 	options := helmValues.Options{
 		ValueFiles: []string{u.ValuesFile},
@@ -54,8 +61,10 @@ func (u *UpgradeRetinaHelmChart) Run() error {
 	if err != nil {
 		return fmt.Errorf("failed to merge values: %w", err)
 	}
+	// logs values to be set during upgrade
+	log.Printf("values to be set during upgrade: %v\n", values)
 
-	rel, err := client.Run(u.ReleaseName, chart, values)
+	rel, err = client.Run(u.ReleaseName, chart, values)
 	if err != nil {
 		return fmt.Errorf("failed to upgrade chart: %w", err)
 	}
