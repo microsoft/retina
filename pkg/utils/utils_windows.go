975e051af172dbaf487a71aabcd795542515e74d
Mathew Merrick <matmerr@microsoft.com>
2024-05-28 18:36:00 +0000

    lints

--- pkg/utils/utils_windows.go
+++ pkg/utils/utils_windows.go
@@ -3,17 +3,13 @@
 package utils
 
 import (
-	"fmt"
-
 	"github.com/cilium/cilium/api/v1/flow"
 )
 
 func GetDropReasonDesc(dr DropReason) flow.DropReason {
-	fmt.Printf("getting drop reason description for %v\n", dr)
-	switch dr {
+	switch dr { //nolint:exhaustive // We are handling all the cases.
 	case DropReason_Drop_INET_FinWait2:
-		fmt.Printf("setting drop as  %v\n", flow.DropReason_TTL_EXCEEDED)
-		return flow.DropReason_TTL_EXCEEDED
+		return flow.DropReason_UNKNOWN_CONNECTION_TRACKING_STATE
 	default:
 		return flow.DropReason_DROP_REASON_UNKNOWN
 	}
