89a6817f05a515c895c7549dfee7e2d35e7fe566
Anubhab Majumdar <anmajumdar@microsoft.com>
2024-06-13 02:57:40 +0000

    Move legacy subcommand to rootCmd
    Signed-off-by: Anubhab Majumdar <anmajumdar@microsoft.com>

--- cmd/root.go
+++ cmd/root.go
@@ -6,17 +6,39 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/microsoft/retina/cmd/legacy"
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "retina-agent",
-	Short: "Retina Agent",
-	Long:  "Start Retina Agent",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-		fmt.Println("Starting Retina Agent")
-	},
+const (
+	configFileName = "/retina/config/config.yaml"
+)
+
+var (
+	metricsAddr          string
+	probeAddr            string
+	enableLeaderElection bool
+	cfgFile              string
+
+	rootCmd = &cobra.Command{
+		Use:   "retina-agent",
+		Short: "Retina Agent",
+		Long:  "Start Retina Agent",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Do Stuff Here
+			fmt.Println("Starting Retina Agent")
+			d := legacy.NewDaemon(metricsAddr, probeAddr, cfgFile, enableLeaderElection)
+			d.Start()
+
+		},
+	}
+)
+
+func init() {
+	rootCmd.Flags().StringVar(&metricsAddr, "metrics-bind-address", ":18080", "The address the metric endpoint binds to.")
+	rootCmd.Flags().StringVar(&probeAddr, "health-probe-bind-address", ":18081", "The address the probe endpoint binds to.")
+	rootCmd.Flags().BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	rootCmd.Flags().StringVar(&cfgFile, "config", configFileName, "config file")
 }
 
 func Execute() {
