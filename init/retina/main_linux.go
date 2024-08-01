ea51e706d6ab0925ab017048cd1460f65232615d
Quang Nguyen <nguyenquang@microsoft.com>
2024-08-01 18:35:39 -0400

    fix: move version and appinsightsID to a centralized location (internal/buildinfo) (#574)
    # Description
    
    As title. These will be populated by the Go compiler via the -ldflags
    during build. Also fixed https://github.com/microsoft/retina/issues/530
    
    ## Related Issue
    
    If this pull request is related to any issue, please mention it here.
    Additionally, make sure that the issue is assigned to you before
    submitting this pull request.
    
    ## Checklist
    
    - [ ] I have read the [contributing
    documentation](https://retina.sh/docs/contributing).
    - [ ] I signed and signed-off the commits (`git commit -S -s ...`). See
    [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [ ] I have correctly attributed the author(s) of the code.
    - [ ] I have tested the changes locally.
    - [ ] I have followed the project's style guidelines.
    - [ ] I have updated the documentation, if necessary.
    - [ ] I have added tests, if applicable.
    
    ## Screenshots (if applicable) or Testing Completed
    
    Please add any relevant screenshots or GIFs to showcase the changes
    made.
    
    ## Additional Notes
    
    Add any additional notes or context about the pull request here.
    
    ---
    
    Please refer to the [CONTRIBUTING.md](../CONTRIBUTING.md) file for more
    information on how to contribute to this project.

--- init/retina/main_linux.go
+++ init/retina/main_linux.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"github.com/microsoft/retina/internal/buildinfo"
 	"github.com/microsoft/retina/pkg/bpf"
 	"github.com/microsoft/retina/pkg/ciliumfs"
 	"github.com/microsoft/retina/pkg/log"
@@ -11,24 +12,16 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	// applicationInsightsID is the instrumentation key for Azure Application Insights
-	// It is set during the build process using the -ldflags flag
-	// If it is set, the application will send telemetry to the corresponding Application Insights resource.
-	applicationInsightsID string
-	version               string
-)
-
 func main() {
 	// Initialize logger
 	opts := log.GetDefaultLogOpts()
 
 	// Enable telemetry if applicationInsightsID is provided
-	if applicationInsightsID != "" {
+	if buildinfo.ApplicationInsightsID != "" {
 		opts.EnableTelemetry = true
-		opts.ApplicationInsightsID = applicationInsightsID
+		opts.ApplicationInsightsID = buildinfo.ApplicationInsightsID
 		// Initialize application insights
-		telemetry.InitAppInsights(applicationInsightsID, version)
+		telemetry.InitAppInsights(buildinfo.ApplicationInsightsID, buildinfo.Version)
 		defer telemetry.ShutdownAppInsights()
 		defer telemetry.TrackPanic()
 	}
@@ -37,7 +30,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	l := zl.Named("init-retina").With(zap.String("version", version))
+	l := zl.Named("init-retina").With(zap.String("version", buildinfo.Version))
 
 	// Setup BPF
 	bpf.Setup(l)
