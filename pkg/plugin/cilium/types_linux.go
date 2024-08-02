6531e1837cbb5e57cb6348199beb56fc8a620ae6
Steven Nguyen <stevennguyen430@gmail.com>
2024-08-02 22:43:06 +0000

    update to add decoder for cilium plugin

--- pkg/plugin/cilium/types_linux.go
+++ pkg/plugin/cilium/types_linux.go
@@ -6,6 +6,9 @@ import (
 	"net"
 
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
+	"github.com/cilium/proxy/pkg/lock"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	kcfg "github.com/microsoft/retina/pkg/config"
 	"github.com/microsoft/retina/pkg/enricher"
 	"github.com/microsoft/retina/pkg/log"
@@ -22,4 +25,25 @@ type cilium struct {
 	enricher        enricher.EnricherInterface
 	externalChannel chan *v1.Event
 	connection      net.Conn
+	p               *parser
+}
+
+type parser struct {
+	l      *log.ZapLogger
+	packet *packet
+}
+
+// re-usable packet to avoid reallocating gopacket datastructures
+type packet struct {
+	lock.Mutex
+	decLayer *gopacket.DecodingLayerParser
+	Layers   []gopacket.LayerType
+	layers.Ethernet
+	layers.IPv4
+	layers.IPv6
+	// layers.ICMPv4
+	// layers.ICMPv6
+	layers.TCP
+	layers.UDP
+	// layers.SCTP
 }
