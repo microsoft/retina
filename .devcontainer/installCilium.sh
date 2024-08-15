388082d788914b2cafef1521fab83ccc3348e2fc
Steven Nguyen <stevennguyen430@gmail.com>
2024-08-15 21:06:53 +0000

    remove dev container updates - this will be a separate pr

--- .devcontainer/installCilium.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -L --remote-name https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
-tar xzvf cilium-linux-amd64.tar.gz
-sudo mv cilium /usr/local/bin/
-rm cilium-linux-amd64.tar.gz
-cilium install
