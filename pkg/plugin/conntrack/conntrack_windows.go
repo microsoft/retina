24eb6b4f687889de0884152fa3ca7374221c4605
Quang Nguyen <nguyenquang@microsoft.com>
2024-08-15 18:18:16 -0400

    windows linter

--- pkg/plugin/conntrack/conntrack_windows.go
+++ pkg/plugin/conntrack/conntrack_windows.go
@@ -1,1 +1,19 @@
 package conntrack
+
+import (
+	"context"
+
+	"github.com/microsoft/retina/pkg/config"
+)
+
+type Conntrack struct{}
+
+// Not implemented for Windows
+func New(_ *config.Config) *Conntrack {
+	return &Conntrack{}
+}
+
+// Not implemented for Windows
+func (c *Conntrack) Run(_ context.Context) error {
+	return nil
+}
