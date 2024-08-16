d36be67e35a013537c669469a317cc1957e56a63
Jacques Massa <jac.massa0908@gmail.com>
2024-08-16 14:45:04 -0400

    fix dynamic error return

--- pkg/managers/watchermanager/watchermanager_test.go
+++ pkg/managers/watchermanager/watchermanager_test.go
@@ -9,10 +9,13 @@ import (
 
 	"github.com/microsoft/retina/pkg/log"
 	mock "github.com/microsoft/retina/pkg/managers/watchermanager/mocks"
-	"github.com/microsoft/retina/pkg/watchers/endpoint"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 	"golang.org/x/sync/errgroup"
+)
+
+var (
+	errInitFailed = errors.New("init failed")
 )
 
 func TestStopWatcherManagerGracefully(t *testing.T) {
@@ -22,13 +25,17 @@ func TestStopWatcherManagerGracefully(t *testing.T) {
 	mgr := NewWatcherManager()
 
 	mockAPIServerWatcher := mock.NewMockIWatcher(ctl)
+	mockEndpointWatcher := mock.NewMockIWatcher(ctl)
 
 	mgr.Watchers = []IWatcher{
-		endpoint.Watcher(),
+		mockEndpointWatcher,
 		mockAPIServerWatcher,
 	}
 
 	mockAPIServerWatcher.EXPECT().Init(gomock.Any()).Return(nil).AnyTimes()
+	mockEndpointWatcher.EXPECT().Init(gomock.Any()).Return(nil).AnyTimes()
+
+	mockEndpointWatcher.EXPECT().Stop(gomock.Any()).Return(nil).AnyTimes()
 	mockAPIServerWatcher.EXPECT().Stop(gomock.Any()).Return(nil).AnyTimes()
 
 	ctx, _ := context.WithCancel(context.Background())
@@ -57,8 +64,8 @@ func TestWatcherInitFailsGracefully(t *testing.T) {
 		mockEndpointWatcher,
 	}
 
-	mockAPIServerWatcher.EXPECT().Init(gomock.Any()).Return(errors.New("error")).AnyTimes()
-	mockEndpointWatcher.EXPECT().Init(gomock.Any()).Return(errors.New("error")).AnyTimes()
+	mockAPIServerWatcher.EXPECT().Init(gomock.Any()).Return(errInitFailed).AnyTimes()
+	mockEndpointWatcher.EXPECT().Init(gomock.Any()).Return(errInitFailed).AnyTimes()
 
 	err := mgr.Start(context.Background())
 	require.NotNil(t, err, "Expected error when starting watcher manager")
