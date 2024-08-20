4d6eb95677a67d0c7af6429e13a96d2b1d060eae
Quang Nguyen <nguyenquang@microsoft.com>
2024-08-20 18:40:47 -0400

    make data aggregation level not enum in header file

--- pkg/plugin/packetparser/_cprog/packetparser.h
+++ pkg/plugin/packetparser/_cprog/packetparser.h
@@ -7,8 +7,5 @@
 // tc-bpf return code to execute the next tc-bpf program.
 #define TC_ACT_UNSPEC   (-1)
 
-typedef enum
-{
-    DATA_AGGREGATION_LEVEL_LOW = 0,
-    DATA_AGGREGATION_LEVEL_HIGH,
-} data_aggregation_level;
+#define DATA_AGGREGATION_LEVEL_LOW 0
+#define DATA_AGGREGATION_LEVEL_HIGH 1
