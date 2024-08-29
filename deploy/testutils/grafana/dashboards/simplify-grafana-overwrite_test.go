599bfef39b88cdca27348b7c4d553ed9a25ca08f
Tisham (whatnick) Dhar <whatnickd@gmail.com>
2024-08-29 21:53:58 +0930

    Refactor to consolidate dashboard tests

---
+++ deploy/testutils/grafana/dashboards/simplify-grafana-overwrite_test.go
@@ -0,0 +1,25 @@
+//go:build dashboard && simplifydashboard
+
+package dashboard
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+// TestOverwriteDashboards simplifies and overwrites Grafana dashboards in deploy folder variants
+func TestOverwriteDashboards(t *testing.T) {
+	// get all json's in various generation deploly folders
+	files, err := filepath.Glob("./*/grafana/dashboards/*.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, file := range files {
+		t.Logf("simplifying/overwriting dashboard: %s", file)
+
+		sourcePath := file
+		_ = SimplifyGrafana(sourcePath, true)
+	}
+}
