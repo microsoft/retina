7b8f0089a14328ec47fb205b3c8e6946124d7747
Quang Nguyen <nguyenquang@microsoft.com>
2024-08-29 17:02:10 -0400

    windows

--- pkg/plugin/conntrack/conntrack_windows.go
+++ pkg/plugin/conntrack/conntrack_windows.go
@@ -1,8 +1,17 @@
 package conntrack
 
+import (
+	"context"
+)
+
 type Conntrack struct{}
 
 // Not implemented for Windows
 func New() *Conntrack {
+	return &Conntrack{}
+}
+
+// Not implemented for Windows
+func (c *Conntrack) Run(_ context.Context) error {
 	return nil
 }
