35c9c0d29e1f3090dc4a9fb5bbe036eda3b61876
Timothy J. Raymond <traymond@microsoft.com>
2024-09-11 09:00:06 -0400

    Add the ability to write config as YAML
    In order to merge configurations, we need to be able to write out the
    resultant configuration as YAML.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,6 +4,7 @@ package config
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"reflect"
@@ -13,6 +14,7 @@ import (
 	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
 
 // Level defines the level of monitor aggregation.
@@ -69,6 +71,10 @@ type Config struct {
 	BypassLookupIPOfInterest bool          `yaml:"bypassLookupIPOfInterest"`
 	DataAggregationLevel     Level         `yaml:"dataAggregationLevel"`
 	MonitorSockPath          string        `yaml:"monitorSockPath"`
+}
+
+func (c *Config) WriteYAML(w io.Writer) error {
+	return yaml.NewEncoder(w).Encode(c)
 }
 
 type FilteredConfig struct {
