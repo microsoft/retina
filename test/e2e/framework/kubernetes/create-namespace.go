81dbc79a3449ad2cc7fcd2627669cda2d2fb7923
Alex Castilio dos Santos <alexsantos@microsoft.com>
2024-10-16 12:11:29 +0100

    test: add steps: ValidateNumOfNodes DeleteNamespace CreateNamespace
    Signed-off-by: Alex Castilio dos Santos <alexsantos@microsoft.com>

---
+++ test/e2e/framework/kubernetes/create-namespace.go
@@ -0,0 +1,59 @@
+package kubernetes
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type CreateNamespace struct {
+	Namespace          string
+	KubeConfigFilePath string
+}
+
+func (c *CreateNamespace) Run() error {
+	config, err := clientcmd.BuildConfigFromFlags("", c.KubeConfigFilePath)
+	if err != nil {
+		return fmt.Errorf("error building kubeconfig: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("error creating Kubernetes client: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeoutSeconds*time.Second)
+	defer cancel()
+
+	_, err = clientset.CoreV1().Namespaces().Create(ctx, c.getNamespace(), metaV1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to create namespace \"%s\": %w", c.Namespace, err)
+	}
+
+	return nil
+}
+
+func (c *CreateNamespace) Stop() error {
+	return nil
+}
+
+func (c *CreateNamespace) Prevalidate() error {
+	return nil
+}
+
+func (c *CreateNamespace) getNamespace() *v1.Namespace {
+	return &v1.Namespace{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Namespace",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name: c.Namespace,
+		},
+	}
+}
---
+++ test/e2e/framework/scaletest/validate-nodes.go
@@ -0,0 +1,65 @@
+package scaletest
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	defaultTimeoutSeconds = 300
+)
+
+type ValidateNumOfNodes struct {
+	NumNodesRequired   int
+	Label              map[string]string
+	KubeConfigFilePath string
+}
+
+// Useful when wanting to do parameter checking, for example
+// if a parameter length is known to be required less than 80 characters,
+// do this here so we don't find out later on when we run the step
+// when possible, try to avoid making external calls, this should be fast and simple
+func (v *ValidateNumOfNodes) Prevalidate() error {
+	return nil
+}
+
+// Primary step where test logic is executed
+// Returning an error will cause the test to fail
+func (v *ValidateNumOfNodes) Run() error {
+	config, err := clientcmd.BuildConfigFromFlags("", v.KubeConfigFilePath)
+	if err != nil {
+		return fmt.Errorf("error building kubeconfig: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("error creating Kubernetes client: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeoutSeconds*time.Second)
+	defer cancel()
+
+	labelSelector := labels.Set(v.Label).String()
+	nodes, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		return errors.Wrap(err, "error getting nodes")
+	}
+
+	if len(nodes.Items) < v.NumNodesRequired {
+		return fmt.Errorf("need %d real nodes to achieve the required max number of pods per node, got %d. Make sure to label nodes with: kubectl label node <name> %s", v.NumNodesRequired, len(nodes.Items), labelSelector)
+	}
+
+	return nil
+}
+
+// Require for background steps
+func (v *ValidateNumOfNodes) Stop() error {
+	return nil
+}
