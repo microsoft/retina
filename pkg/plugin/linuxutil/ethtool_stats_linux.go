3945951ef932e04dccc6c0991706ef49f1c82602
Beegii Khurelsukh <72892628+BeegiiK@users.noreply.github.com>
2024-11-27 15:51:57 +0000

    fix(metric): Update dynamic interface label to be constant (#1078)
    # Description
    
    Updated dynamic interface label for `interface_stats` metric to be set
    to `all_interfaces`. Also did some optimisation on the `EthtoolStats`
    struct. All stat names will be accumulated across all interface names
    
    ## Related Issue
    
    https://github.com/microsoft/retina/issues/1067
    
    ## Checklist
    
    - [x] I have read the [contributing
    documentation](https://retina.sh/docs/contributing).
    - [x] I signed and signed-off the commits (`git commit -S -s ...`). See
    [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [x] I have correctly attributed the author(s) of the code.
    - [x] I have tested the changes locally.
    - [x] I have followed the project's style guidelines.
    - [x] I have updated the documentation, if necessary.
    - [ ] I have added tests, if applicable.
    
    ## Screenshots (if applicable) or Testing Completed
    
    Please add any relevant screenshots or GIFs to showcase the changes
    made.
    
    ## Additional Notes
    
    Add any additional notes or context about the pull request here.
    
    ---
    
    Please refer to the [CONTRIBUTING.md](../CONTRIBUTING.md) file for more
    information on how to contribute to this project.

--- pkg/plugin/linuxutil/ethtool_stats_linux.go
+++ pkg/plugin/linuxutil/ethtool_stats_linux.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/microsoft/retina/pkg/log"
 	"github.com/microsoft/retina/pkg/metrics"
+	"github.com/microsoft/retina/pkg/utils"
 	"github.com/safchain/ethtool"
 	"go.uber.org/zap"
 )
@@ -60,7 +61,7 @@ func (er *EthtoolReader) readInterfaceStats() error {
 	defer er.ethHandle.Close()
 
 	er.data = &EthtoolStats{
-		stats: make(map[string]map[string]uint64),
+		stats: make(map[string]uint64),
 	}
 
 	for _, i := range ifaces {
@@ -83,12 +84,12 @@ func (er *EthtoolReader) readInterfaceStats() error {
 			continue
 		}
 
-		er.data.stats[i.Name] = make(map[string]uint64)
 		tempMap := er.processStats(ifaceStats)
-		er.data.stats[i.Name] = tempMap
+		for key, value := range tempMap {
+			er.data.stats[key] += value
+		}
 
 		er.l.Debug("Processed ethtool Stats ", zap.String("ifacename", i.Name))
-
 	}
 
 	return nil
@@ -114,10 +115,8 @@ func (er *EthtoolReader) processStats(ifaceStats map[string]uint64) map[string]u
 
 func (er *EthtoolReader) updateMetrics() {
 	// update metrics section
-	// retrive interfacename and statname from ethStats
-	for ifName, stats := range er.data.stats {
-		for statName, statVal := range stats {
-			metrics.InterfaceStatsGauge.WithLabelValues(ifName, statName).Set(float64(statVal))
-		}
+	// retrive statname from ethStats
+	for statName, statVal := range er.data.stats {
+		metrics.InterfaceStatsGauge.WithLabelValues(utils.InterfaceNameConstant, statName).Set(float64(statVal))
 	}
 }
