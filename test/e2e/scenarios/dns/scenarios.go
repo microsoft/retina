988dfb77d0b06e631ea5f54c38fecc514bc48071
Jacques Massa <jac.massa0908@gmail.com>
2024-12-11 17:37:00 -0500

    refactor

--- test/e2e/scenarios/dns/scenarios.go
+++ test/e2e/scenarios/dns/scenarios.go
@@ -36,7 +36,7 @@ type ResponseValidationParams struct {
 }
 
 // ValidateBasicDNSMetrics validates basic DNS metrics present in the metrics endpoint
-func ValidateBasicDNSMetrics(scenarioName string, req *RequestValidationParams, resp *ResponseValidationParams, namespace, arch string) *types.Scenario {
+func ValidateBasicDNSMetrics(scenarioName string, req *RequestValidationParams, resp *ResponseValidationParams, kubeConfigFilePath, namespace, arch string) *types.Scenario {
 	// generate a random ID using rand
 	id := fmt.Sprintf("basic-dns-port-forward-%d", rand.Int()) // nolint:gosec // fine to use math/rand here
 	agnhostName := "agnhost-" + id
