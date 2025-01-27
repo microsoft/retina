5b54a471b6383bdf54c03e470dfb4d24b8293e47
Simone Rodigari <srodigari@microsoft.com>
2025-01-27 15:38:44 +0000

    test(mc): update integration test for retina-kind

--- test/multicloud/examples/integration/retina-kind/main.tf
+++ test/multicloud/examples/integration/retina-kind/main.tf
@@ -8,3 +8,19 @@ module "retina" {
   source     = "../../../modules/retina"
   retina_version = var.retina_version
 }
+
+output "host" {
+  value = module.kind.host
+}
+
+output "cluster_ca_certificate" {
+  value = module.kind.cluster_ca_certificate
+}
+
+output "client_certificate" {
+  value = module.kind.client_certificate
+}
+
+output "client_key" {
+  value = module.kind.client_key
+}
--- test/multicloud/test/integration_retina_kind_test.go
+++ test/multicloud/test/integration_retina_kind_test.go
@@ -25,4 +25,23 @@ func TestRetinaKindIntegration(t *testing.T) {
 	terraform.Apply(t, opts)
 
 	// TODO: add actual tests here
+	// test the cluster is accessible with the ca cert, client cert and client key
+	caCert := terraform.Output(t, opts, "cluster_ca_certificate")
+	clientCert := terraform.Output(t, opts, "client_certificate")
+	clientKey := terraform.Output(t, opts, "client_key")
+	host := terraform.Output(t, opts, "host")
+
+	// test the cluster is accessible with the ca cert, client cert and client key
+	checkClusterAccess(t, caCert, clientCert, clientKey, host)
+
+	// create a Kubernetes clientset
+	clientSet, err := buildClientSet(caCert, clientCert, clientKey, host)
+	if err != nil {
+		t.Fatalf("Failed to create Kubernetes clientset: %v", err)
+	}
+
+	// check the retina pods logs for errors
+	checkRetinaLogs(t, clientSet)
+
+	// TODO: add more tests here
 }
