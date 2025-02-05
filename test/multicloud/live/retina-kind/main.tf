a1db7612cad00ba7cc98aded2dcb4751987f2581
Simone Rodigari <srodigari@microsoft.com>
2025-02-05 09:06:34 +0000

    test(mc): Update stacks under live/

--- test/multicloud/live/retina-kind/main.tf
+++ test/multicloud/live/retina-kind/main.tf
@@ -4,6 +4,11 @@ module "kind" {
 }
 
 module "retina" {
-  depends_on = [module.kind]
-  source     = "../../modules/retina"
+  depends_on     = [module.kind]
+  source         = "../../modules/helm-release"
+  release_name   = var.retina_release_name
+  repository_url = var.retina_repository_url
+  chart_version  = var.retina_chart_version
+  chart_name     = var.retina_chart_name
+  values         = var.retina_values
 }
