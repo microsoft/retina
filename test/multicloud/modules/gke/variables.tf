360aa013920eec6642fd5595b438ea66befe3109
Simone Rodigari <srodigari@microsoft.com>
2025-02-06 20:39:11 +0000

    test(mc): Clean-up

--- test/multicloud/modules/gke/variables.tf
+++ test/multicloud/modules/gke/variables.tf
@@ -23,39 +23,3 @@ variable "subnet_cidr" {
   type        = string
   default     = "10.0.0.0/24"
 }
-
-variable "inbound_protocol" {
-  description = "Protocol for inbound firewall rule"
-  type        = string
-  default     = "tcp"
-}
-
-variable "inbound_ports" {
-  description = "Ports for inbound firewall rule"
-  type        = list(string)
-  default     = []
-}
-
-variable "inbound_source_ranges" {
-  description = "Source IP ranges for inbound firewall rule"
-  type        = list(string)
-  default     = []
-}
-
-variable "outbound_protocol" {
-  description = "Protocol for outbound firewall rule"
-  type        = string
-  default     = "tcp"
-}
-
-variable "outbound_ports" {
-  description = "Ports for outbound firewall rule"
-  type        = list(string)
-  default     = []
-}
-
-variable "outbound_destination_ranges" {
-  description = "Destination IP ranges for outbound firewall rule"
-  type        = list(string)
-  default     = []
-}
--- test/multicloud/modules/grafana/variables.tf
+++ test/multicloud/modules/grafana/variables.tf
@@ -1,8 +1,4 @@
 variable "prometheus_endpoints" {
   description = "A map of Prometheus endpoints to add as data sources."
   type        = map(string)
-  default = {
-    aks  = "http://85.210.188.53:9090"
-    kind = "http://127.0.0.1:9090"
-  }
 }
