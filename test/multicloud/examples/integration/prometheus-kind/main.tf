22afb98943a8579933abf9744e082ba9bba4c108
Simone Rodigari <srodigari@microsoft.com>
2025-02-07 15:29:56 +0000

    test(mc): prometheus integration test to fetch local yaml

--- test/multicloud/examples/integration/prometheus-kind/main.tf
+++ test/multicloud/examples/integration/prometheus-kind/main.tf
@@ -3,8 +3,21 @@ module "kind" {
   prefix = var.prefix
 }
 
-module "prometheus" {
+module "retina" {
   depends_on     = [module.kind]
+  source         = "../../../modules/helm-release"
+  release_name   = var.retina_release_name
+  repository_url = var.retina_repository_url
+  chart_version  = var.retina_chart_version
+  chart_name     = var.retina_chart_name
+  values         = var.retina_values
+}
+
+module "prometheus" {
+  depends_on     = [
+    module.kind,
+    module.retina
+  ]
   source         = "../../../modules/helm-release"
   release_name   = var.prometheus_release_name
   repository_url = var.prometheus_repository_url
