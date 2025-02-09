924e8c5b4842aa4d3bff8f0bb998448e1f780b6d
manojks1999 <9743manoj@gmail.com>
2025-02-10 00:37:19 +0530

    fix(windows): added dummy set method

--- pkg/plugin/conntrack/conntrack_windows.go
+++ pkg/plugin/conntrack/conntrack_windows.go
@@ -2,6 +2,8 @@ package conntrack
 
 import (
 	"context"
+
+	"github.com/microsoft/retina/pkg/config"
 )
 
 type Conntrack struct{}
@@ -14,4 +16,9 @@ func New() (*Conntrack, error) {
 // Not implemented for Windows
 func (c *Conntrack) Run(_ context.Context) error {
 	return nil
+}
+
+// SetConfig sets the config after initialization
+func (c *Conntrack) SetConfig(_ *config.Config) {
+	// No-op for Windows
 }
