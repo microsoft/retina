08fd89ba3ef3d54ae16ba6b978591f5f7f5216ff
Amilcar Aponte Jimenez <apontejaj@gmail.com>
2025-02-13 14:28:28 +0000

    Updating grafana dashboards

--- deploy/testutils/grafana/dashboards/simplify-grafana_test.go
+++ deploy/testutils/grafana/dashboards/simplify-grafana_test.go
@@ -11,10 +11,14 @@ import (
 // TestDashboardsAreSimplified ensures that all dashboards are simplified
 func TestDashboardsAreSimplified(t *testing.T) {
 	// get all json's in this folder
-	files, err := filepath.Glob("../../../*/grafana/dashboards/*.json")
+	files, err := filepath.Glob("../../../grafana-dashboards/*.json")
 
 	if err != nil {
 		t.Fatal(err)
+	}
+	
+	if len(files) == 0 {
+		t.Fatal("No JSON files found")
 	}
 
 	for _, file := range files {
