f3879601528a88168a0606bab7388e857cce5095
beegiik <kbeegii@gmail.com>
2025-03-06 12:27:49 +0000

    Refactoring

--- cmd/standalone_daemon.go
+++ cmd/standalone_daemon.go
@@ -6,11 +6,9 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"strings"
 
-	"github.com/microsoft/retina/internal/buildinfo"
+	"github.com/microsoft/retina/cmd/telemetry"
 	"github.com/microsoft/retina/pkg/log"
-	"github.com/microsoft/retina/pkg/telemetry"
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 
@@ -21,75 +19,50 @@ import (
 )
 
 type StandaloneDaemon struct {
-	l             *log.ZapLogger
+	config        *config.Config
 	cache         *cache.StandaloneCache
 	httpServer    *sm.HTTPServer
 	pluginManager *pm.PluginManager
-	config        *config.Config
-	tel           telemetry.Telemetry
 }
 
-func NewStandaloneDaemon(cfg *config.Config, zl *log.ZapLogger) (*StandaloneDaemon, error) {
-	fmt.Println("starting Standalone Retina daemon")
-	sdLogger := zl.Named("standalone-daemon")
+func NewStandaloneDaemon(daemonCfg *config.Config) *StandaloneDaemon {
+	return &StandaloneDaemon{
+		config: daemonCfg,
+	}
+}
 
-	var tel telemetry.Telemetry
-	var err error
-	if cfg.EnableTelemetry {
-		if buildinfo.ApplicationInsightsID == "" {
-			panic("telemetry enabled, but ApplicationInsightsID is empty")
-		}
-		sdLogger.Info("telemetry enabled", zap.String("applicationInsightsID", buildinfo.ApplicationInsightsID))
-		tel, err = telemetry.NewAppInsightsTelemetryClient("standalone-retina-agent", map[string]string{
-			"version": buildinfo.Version,
-			"plugins": strings.Join(cfg.EnabledPlugin, `,`),
-		})
-		if err != nil {
-			sdLogger.Error("failed to create telemetry client", zap.Error(err))
-			return nil, fmt.Errorf("error when creating telemetry client: %w", err)
-		}
-	} else {
-		sdLogger.Info("telemetry disabled")
-		tel = telemetry.NewNoopTelemetry()
+func (sd *StandaloneDaemon) Start(zl *log.ZapLogger) error {
+	fmt.Println("starting standalone Retina daemon")
+	mainLogger := zl.Named("standalone-daemon").Sugar()
+
+	tel, err := telemetry.InitializeTelemetryClient(nil, sd.config, mainLogger)
+	if err != nil {
+		return fmt.Errorf("failed to initialize telemetry client: %w", err)
 	}
 
-	pMgr, err := pm.NewPluginManager(
-		cfg,
+	sd.pluginManager, err = pm.NewPluginManager(
+		sd.config,
 		tel,
 	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create plugin manager: %w", err)
+		return fmt.Errorf("failed to create plugin manager: %w", err)
 	}
 
-	// create HTTP server for API server
-	httpServer := sm.NewHTTPServer(
-		cfg.APIServer.Host,
-		cfg.APIServer.Port,
+	sd.httpServer = sm.NewHTTPServer(
+		sd.config.APIServer.Host,
+		sd.config.APIServer.Port,
 	)
 
-	return &StandaloneDaemon{
-		l:             sdLogger,
-		httpServer:    httpServer,
-		pluginManager: pMgr,
-		config:        cfg,
-		tel:           tel,
-	}, nil
-}
-
-func (sd *StandaloneDaemon) Start() {
-	sd.l.Info("Starting standalone daemon")
-
-	// Start the HTTP server and initialize the cache
 	if err := sd.httpServer.Init(); err != nil {
-		sd.l.Error("failed to start http server")
+		mainLogger.Fatal("failed to start http server", zap.Error(err))
 	}
-	sd.cache = cache.NewStandaloneCache()
+	defer sd.pluginManager.Stop()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// start heartbeat goroutine for application insights
-	go sd.tel.Heartbeat(ctx, sd.config.TelemetryInterval)
+	go tel.Heartbeat(ctx, sd.config.TelemetryInterval)
 
 	var g *errgroup.Group
 	g, ctx = errgroup.WithContext(ctx)
@@ -102,14 +75,11 @@ func (sd *StandaloneDaemon) Start() {
 	})
 
 	if err := g.Wait(); err != nil {
-		sd.l.Panic("Error running standalone daemon", zap.Error(err))
+		mainLogger.Panic("Error running standalone daemon", zap.Error(err))
 	}
 
-	sd.l.Info("Started standalone daemon")
-}
+	sd.cache = cache.NewStandaloneCache()
+	mainLogger.Info("Started standalone daemon")
 
-func (sd *StandaloneDaemon) Stop() {
-	// Clean up plugin resources
-	sd.pluginManager.Stop()
-	sd.l.Info("Stopped the standalone daemon")
+	return nil
 }
