3a6d0a2f56fd6c3b871374dad93afb6a23a67590
beegiik <kbeegii@gmail.com>
2025-03-09 17:48:25 +0000

    Add enrichment logic and additional unit tests

--- pkg/controllers/cache/standalone_cache.go
+++ pkg/controllers/cache/standalone_cache.go
@@ -10,34 +10,60 @@ import (
 	"go.uber.org/zap"
 )
 
-type StandaloneCache struct {
-	rwMutex sync.RWMutex
-	l       *log.ZapLogger
-	ipToPod map[string]PodInfo
-}
+var (
+	MaxStandaloneCacheEventSize = 250
+)
+
+type Action string
+
+const (
+	EventAdd    Action = "Add"
+	EventDelete Action = "Delete"
+)
 
 type PodInfo struct {
 	Name      string
 	Namespace string
 }
 
+type StandaloneCacheEvent struct {
+	Ip      string
+	PodInfo PodInfo
+	Action  Action
+}
+
+type StandaloneCache struct {
+	rwMutex      sync.RWMutex
+	l            *log.ZapLogger
+	ipToPod      map[string]PodInfo
+	eventChannel chan StandaloneCacheEvent
+}
+
 func NewStandaloneCache() *StandaloneCache {
 	return &StandaloneCache{
-		l:       log.Logger().Named(string("Standalone Cache")),
-		ipToPod: make(map[string]PodInfo),
+		l:            log.Logger().Named(string("Standalone Cache")),
+		ipToPod:      make(map[string]PodInfo),
+		eventChannel: make(chan StandaloneCacheEvent, MaxStandaloneCacheEventSize),
 	}
 }
 
-// AddPod adds pods IP mapping to the cache
 func (c *StandaloneCache) AddPod(ip, name, namespace string) {
 	c.rwMutex.Lock()
 	defer c.rwMutex.Unlock()
 
 	c.ipToPod[ip] = PodInfo{Name: name, Namespace: namespace}
 	c.l.Info("Added pod", zap.String("ip", ip), zap.String("name", name), zap.String("namespace", namespace))
+
+	c.eventChannel <- StandaloneCacheEvent{
+		Ip: ip,
+		PodInfo: PodInfo{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Action: EventAdd,
+	}
 }
 
-// GetPod retrieves pod information by IP address
 func (c *StandaloneCache) GetPod(ip string) *PodInfo {
 	c.rwMutex.RLock()
 	defer c.rwMutex.RUnlock()
@@ -48,7 +74,6 @@ func (c *StandaloneCache) GetPod(ip string) *PodInfo {
 	return nil
 }
 
-// DeletePod removes pods IP mapping from the cache
 func (c *StandaloneCache) DeletePod(ip string) {
 	c.rwMutex.Lock()
 	defer c.rwMutex.Unlock()
@@ -56,5 +81,15 @@ func (c *StandaloneCache) DeletePod(ip string) {
 	if podInfo, exists := c.ipToPod[ip]; exists {
 		delete(c.ipToPod, ip)
 		c.l.Info("Deleted pod", zap.String("ip", ip), zap.String("name", podInfo.Name), zap.String("namespace", podInfo.Namespace))
+
+		c.eventChannel <- StandaloneCacheEvent{
+			Ip:      ip,
+			PodInfo: podInfo,
+			Action:  EventDelete,
+		}
 	}
+}
+
+func (c *StandaloneCache) WatchEvents() <-chan StandaloneCacheEvent {
+	return c.eventChannel
 }
