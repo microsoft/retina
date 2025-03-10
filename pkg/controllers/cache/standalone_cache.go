20f4de2bd5eeeb5843f6289d37b0ed9e1548ea37
beegiik <kbeegii@gmail.com>
2025-03-10 17:52:51 +0000

    Re-structure logic - need to polish in next commit

--- pkg/controllers/cache/standalone_cache.go
+++ pkg/controllers/cache/standalone_cache.go
@@ -4,23 +4,10 @@
 package cache
 
 import (
-	"errors"
 	"sync"
 
 	"github.com/microsoft/retina/pkg/log"
 	"go.uber.org/zap"
-)
-
-var (
-	MaxStandaloneCacheEventSize = 250
-	ErrEventChannelFull         = errors.New("event channel is full, event dropped")
-)
-
-type Action string
-
-const (
-	EventAdd    Action = "Add"
-	EventDelete Action = "Delete"
 )
 
 type PodInfo struct {
@@ -28,34 +15,20 @@ type PodInfo struct {
 	Namespace string
 }
 
-type StandaloneCacheEvent struct {
-	Ip     string
-	Action Action
-}
-
 type StandaloneCache struct {
-	rwMutex      sync.RWMutex
-	l            *log.ZapLogger
-	ipToPod      map[string]PodInfo
-	eventChannel chan StandaloneCacheEvent
+	rwMutex sync.RWMutex
+	l       *log.ZapLogger
+	ipToPod map[string]PodInfo
 }
 
 func NewStandaloneCache() *StandaloneCache {
 	return &StandaloneCache{
-		l:            log.Logger().Named(string("standalone-cache")),
-		ipToPod:      make(map[string]PodInfo),
-		eventChannel: make(chan StandaloneCacheEvent, MaxStandaloneCacheEventSize),
+		l:       log.Logger().Named(string("standalone-cache")),
+		ipToPod: make(map[string]PodInfo),
 	}
 }
 
-func (c *StandaloneCache) AddPod(ip, name, namespace string) {
-	c.rwMutex.Lock()
-	defer c.rwMutex.Unlock()
-
-	c.ipToPod[ip] = PodInfo{Name: name, Namespace: namespace}
-	c.l.Info("Added pod", zap.String("ip", ip), zap.String("name", name), zap.String("namespace", namespace))
-}
-
+// ?
 func (c *StandaloneCache) GetPod(ip string) *PodInfo {
 	c.rwMutex.RLock()
 	defer c.rwMutex.RUnlock()
@@ -66,26 +39,28 @@ func (c *StandaloneCache) GetPod(ip string) *PodInfo {
 	return nil
 }
 
-func (c *StandaloneCache) DeletePod(ip string) {
+func (c *StandaloneCache) ProcessPodInfo(ip string, podInfo *PodInfo) {
+	if podInfo != nil {
+		c.addPod(ip, podInfo.Name, podInfo.Namespace)
+	} else {
+		c.deletePod(ip)
+	}
+}
+
+func (c *StandaloneCache) addPod(ip, name, namespace string) {
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
+
+	c.ipToPod[ip] = PodInfo{Name: name, Namespace: namespace}
+	c.l.Debug("Added pod to cache", zap.String("ip", ip), zap.String("name", name), zap.String("namespace", namespace))
+}
+
+func (c *StandaloneCache) deletePod(ip string) {
 	c.rwMutex.Lock()
 	defer c.rwMutex.Unlock()
 
 	if podInfo, exists := c.ipToPod[ip]; exists {
 		delete(c.ipToPod, ip)
-		c.l.Info("Deleted pod", zap.String("ip", ip), zap.String("name", podInfo.Name), zap.String("namespace", podInfo.Namespace))
-	}
-}
-
-func (c *StandaloneCache) WatchEvents() <-chan StandaloneCacheEvent {
-	return c.eventChannel
-}
-
-func (c *StandaloneCache) PublishEvent(ip string, action Action) error {
-	select {
-	case c.eventChannel <- StandaloneCacheEvent{Ip: ip, Action: action}:
-		return nil
-	default:
-		c.l.Warn("Event channel full, dropping event", zap.String("ip", ip), zap.String("action", string(action)))
-		return ErrEventChannelFull
+		c.l.Info("Deleted pod from cache", zap.String("ip", ip), zap.String("name", podInfo.Name), zap.String("namespace", podInfo.Namespace))
 	}
 }
