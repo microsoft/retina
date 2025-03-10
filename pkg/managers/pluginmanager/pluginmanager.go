6d918c8c9b616708b4db39f570a4d17c1e9fe8b0
manojks1999 <64463831+manojks1999@users.noreply.github.com>
2025-03-10 21:20:35 +0530

    fix: fix Conntrack GC running in Retina default (#1323)
    # Description
    
    Please provide a brief description of the changes made in this pull
    request.
    
    ## Related Issue
    
    #1314 
    
    Below are changes made in current PR:
    * Added a condition of check before creating a instance
    * Check is made by the configs of instance
    * Added with appropriate log
    
    ## Checklist
    
    - [ * ] I have read the [contributing
    documentation](https://retina.sh/docs/contributing).
    - [ * ] I signed and signed-off the commits (`git commit -S -s ...`).
    See [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [ * ] I have correctly attributed the author(s) of the code.
    - [ * ] I have tested the changes locally.
    - [ * ] I have followed the project's style guidelines.
    - [ * ] I have updated the documentation, if necessary.
    - [ * ] I have added tests, if applicable.
    
    ---------
    
    Co-authored-by: Simone Rodigari <32323373+SRodi@users.noreply.github.com>

--- pkg/managers/pluginmanager/pluginmanager.go
+++ pkg/managers/pluginmanager/pluginmanager.go
@@ -136,15 +136,19 @@ func (p *PluginManager) Start(ctx context.Context) error {
 	}
 
 	g, ctx := errgroup.WithContext(ctx)
-
-	// run conntrack GC
-	ct, err := conntrack.New()
-	if err != nil {
-		return errors.Wrap(err, "failed to get conntrack instance")
+	_, isPacketParserEnabled := p.plugins["packetparser"]
+	// run conntrack GC only if packetparser is enabled
+	if isPacketParserEnabled {
+		ct, connErr := conntrack.New()
+		if connErr != nil {
+			return errors.Wrap(connErr, "failed to get conntrack instance")
+		}
+		g.Go(func() error {
+			return errors.Wrapf(ct.Run(ctx), "failed to run conntrack GC")
+		})
+	} else {
+		p.l.Info("Skipping Conntrack GC loop as packetparser plugin is not enabled")
 	}
-	g.Go(func() error {
-		return errors.Wrapf(ct.Run(ctx), "failed to run conntrack GC")
-	})
 
 	// start all plugins
 	for _, plugin := range p.plugins {
