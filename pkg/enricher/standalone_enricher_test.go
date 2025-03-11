0d85fde7b74c69ce413fe26e1d82bee0aabb7471
beegiik <kbeegii@gmail.com>
2025-03-11 11:44:45 +0000

    updated tests to align with updated code

--- pkg/enricher/standalone_enricher_test.go
+++ pkg/enricher/standalone_enricher_test.go
@@ -6,8 +6,10 @@ package enricher
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/microsoft/retina/pkg/controllers/cache"
+	"github.com/microsoft/retina/pkg/enricher/statefile"
 	"github.com/microsoft/retina/pkg/log"
 	"github.com/stretchr/testify/assert"
 )
@@ -23,39 +25,58 @@ func TestStandaloneEnricher(t *testing.T) {
 	if _, err := log.SetupZapLogger(log.GetDefaultLogOpts()); err != nil {
 		t.Fatalf("Failed to setup logger: %v", err)
 	}
-
 	testCache := cache.NewStandaloneCache()
 	enricher := NewStandaloneEnricher(context.Background(), testCache)
 
 	go enricher.Run(context.Background())
 
-	// Event add for pod that doesn't exist in statefile
-	enricher.ProcessEvent(invalidIp, cache.EventAdd)
-	podInfo := testCache.GetPod(invalidIp)
+	// Enrich pod not in statefile
+	err := enricher.PublishEvent(invalidIp)
+	assert.NoError(t, err)
+
+	podInfo := enricher.GetPodInfo(invalidIp)
 	assert.Nil(t, podInfo)
 
-	// Event add for pod that exists in statefile
-	enricher.ProcessEvent(ip, cache.EventAdd)
+	// Enrich pod in statefile
+	err = enricher.PublishEvent(ip)
+	assert.NoError(t, err)
 
-	podInfo = testCache.GetPod(ip)
-	assert.NotNil(t, podInfo)
+	assert.Eventually(t, func() bool {
+		podInfo = enricher.GetPodInfo(ip)
+		return podInfo != nil
+	}, 100*time.Millisecond, 25*time.Millisecond, "Pod should be available in cache")
 	assert.Equal(t, name, podInfo.Name)
 	assert.Equal(t, namespace, podInfo.Namespace)
 
-	// Event delete for pod that doesn't exist in cache
-	enricher.ProcessEvent(invalidIp, cache.EventDelete)
-	podInfo = testCache.GetPod(invalidIp)
+	// Delete pod not in statefile
+	statefile.State_file_location = "/home/beegii/src/retina/pkg/enricher/statefile/mock_statefile_updated.json"
+	err = enricher.PublishEvent(ip)
+	assert.NoError(t, err)
+
+	assert.Eventually(t, func() bool {
+		podInfo = enricher.GetPodInfo(ip)
+		return podInfo == nil
+	}, 100*time.Millisecond, 25*time.Millisecond, "Pod should be deleted from cache")
 	assert.Nil(t, podInfo)
 
-	// Test deleting a pod that exists in the cache
-	enricher.ProcessEvent(ip, cache.EventDelete)
-	podInfo = testCache.GetPod(ip)
-	assert.Nil(t, podInfo) // cache should be empty now
+	enricher.Stop()
+}
 
-	// Test unknown event action
-	unknownAction := cache.Action("unknown")
-	enricher.ProcessEvent("9.9.9.9", unknownAction)
-	assert.Nil(t, testCache.GetPod("9.9.9.9")) // nothing should happen
+func TestPublishEvent(t *testing.T) {
+	if _, err := log.SetupZapLogger(log.GetDefaultLogOpts()); err != nil {
+		t.Fatalf("Failed to setup logger: %v", err)
+	}
+
+	MaxStandaloneCacheEventSize = 1
+	testCache := cache.NewStandaloneCache()
+	enricher := NewStandaloneEnricher(context.Background(), testCache)
+
+	go enricher.Run(context.Background())
+
+	err1 := enricher.PublishEvent(ip)
+	err2 := enricher.PublishEvent(invalidIp)
+	assert.NoError(t, err1)
+	assert.Equal(t, err2, ErrEventChannelFull)
 
 	enricher.Stop()
 }
