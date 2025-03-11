b022df27ba03a5abed5766174702230e4b21dfb2
beegiik <kbeegii@gmail.com>
2025-03-11 15:51:31 +0000

    bullet tracer to confirm end to end

--- pkg/enricher/types.go
+++ pkg/enricher/types.go
@@ -3,11 +3,8 @@
 package enricher
 
 import (
-	"context"
-
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	"github.com/cilium/cilium/pkg/hubble/container"
-	"github.com/microsoft/retina/pkg/log"
 )
 
 //go:generate go run go.uber.org/mock/mockgen@v0.4.0 -destination=mock_enricherinterface.go  -copyright_file=../lib/ignore_headers.txt -package=enricher github.com/microsoft/retina/pkg/enricher EnricherInterface
@@ -16,9 +13,4 @@ type EnricherInterface interface {
 	Run()
 	Write(ev *v1.Event)
 	ExportReader() *container.RingReader
-}
-
-type GenericEnricher struct {
-	ctx context.Context
-	l   *log.ZapLogger
 }
