24a5f59fe9cf5c33b2430cab2b04267cb5c5ac73
beegiik <kbeegii@gmail.com>
2025-04-09 16:32:28 +0100

    Reuse http server and plugin manager via controller manager

--- cmd/standalone/daemon.go
+++ cmd/standalone/daemon.go
@@ -4,7 +4,6 @@
 package standalone
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/microsoft/retina/cmd/utils"
@@ -12,10 +11,11 @@ import (
 	"github.com/microsoft/retina/pkg/log"
 	"github.com/microsoft/retina/pkg/metrics"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
+	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/microsoft/retina/pkg/config"
 	"github.com/microsoft/retina/pkg/controllers/cache"
+	cm "github.com/microsoft/retina/pkg/managers/controllermanager"
 	pm "github.com/microsoft/retina/pkg/managers/pluginmanager"
 	sm "github.com/microsoft/retina/pkg/managers/servermanager"
 )
@@ -43,48 +43,32 @@ func (d *Daemon) Start(zl *log.ZapLogger) error {
 		return fmt.Errorf("failed to initialize telemetry client: %w", err)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	ctx := ctrl.SetupSignalHandler() // Importing K8s signal handler - ok?
 
 	cache := cache.NewStandaloneCache()
 	enrich := enricher.NewStandaloneEnricher(ctx, cache, d.config)
 	enrich.Run()
 
-	d.pluginManager, err = pm.NewPluginManager(
-		d.config,
-		tel,
-	)
+	// metrics module?
+	// next part
+
+	// enable pod level needs to be false!
+	controllerMgr, err := cm.NewControllerManager(d.config, nil, tel)
 	if err != nil {
-		return fmt.Errorf("failed to create plugin manager: %w", err)
+		mainLogger.Fatal("Failed to create controller manager", zap.Error(err))
 	}
-
-	d.httpServer = sm.NewHTTPServer(
-		d.config.APIServer.Host,
-		d.config.APIServer.Port,
-	)
-
-	if err := d.httpServer.Init(); err != nil {
-		mainLogger.Fatal("Failed to start http server", zap.Error(err))
+	if err := controllerMgr.Init(ctx); err != nil {
+		mainLogger.Fatal("Failed to initialize controller manager", zap.Error(err))
 	}
-	defer d.pluginManager.Stop()
+	defer controllerMgr.Stop(ctx)
 
 	// start heartbeat goroutine for application insights
 	go tel.Heartbeat(ctx, d.config.TelemetryInterval)
 
-	var g *errgroup.Group
-	g, ctx = errgroup.WithContext(ctx)
+	// Start controller manager, which will start the http server and plugin manager
+	go controllerMgr.Start(ctx)
+	mainLogger.Info("Started controller manager")
 
-	g.Go(func() error {
-		return d.pluginManager.Start(ctx)
-	})
-	g.Go(func() error {
-		return d.httpServer.Start(ctx)
-	})
-
-	if err := g.Wait(); err != nil {
-		mainLogger.Panic("Error running standalone daemon", zap.Error(err))
-	}
-
-	mainLogger.Info("Started standalone daemon")
+	mainLogger.Info("Network observability exiting. Till next time!")
 	return nil
 }
