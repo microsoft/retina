3dd0c271606e7dc344194de97825ed4235b82789
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-05-05 16:25:19 -0700

    feat(plugin): ebpfwindows plugin + retina ctrl plane changes

---
+++ pkg/plugin/ebpfwindows/eventsmap_windows.go
@@ -0,0 +1,78 @@
+package ebpfwindows
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	registerEventsMapCallback   = retinaEbpfAPI.NewProc("RetinaRegisterEventsMapCallback")
+	unregisterEventsMapCallback = retinaEbpfAPI.NewProc("RetinaUnregisterEventsMapCallback")
+)
+
+type eventsMapCallback func(data unsafe.Pointer, size uint32)
+
+// Callbacks in Go can only be passed as functions with specific signatures and often need to be wrapped in a syscall-compatible function.
+var eventsCallback eventsMapCallback
+
+// This function will be passed to the Windows API
+func eventsMapSysCallCallback(data unsafe.Pointer, size uint32) int {
+
+	if eventsCallback != nil {
+		eventsCallback(data, size)
+	}
+
+	return 0
+}
+
+// EventsMap interface represents a events map
+type EventsMap interface {
+	RegisterForCallback(eventsMapCallback) error
+	UnregisterForCallback() error
+}
+
+type eventsMap struct {
+	perfBuffer uintptr
+}
+
+// NewEventsMap creates a new metrics map
+func NewEventsMap() EventsMap {
+	return &eventsMap{perfBuffer: 0}
+}
+
+// RegisterForCallback registers a callback function to be called when a new event is added to the events map
+func (e *eventsMap) RegisterForCallback(cb eventsMapCallback) error {
+
+	eventsCallback = cb
+
+	fmt.Println("Attempting to register")
+	// Convert the Go function into a syscall-compatible function
+	callback := syscall.NewCallback(eventsMapSysCallCallback)
+
+	// Call the API
+	ret, _, err := registerEventsMapCallback.Call(
+		uintptr(callback),
+		uintptr(unsafe.Pointer(&e.perfBuffer)),
+	)
+
+	if ret != 0 {
+		fmt.Println("Error registering for events map callback")
+		return err
+	}
+
+	return nil
+}
+
+// UnregisterForCallback unregisters the callback function
+func (e *eventsMap) UnregisterForCallback() error {
+
+	// Call the API
+	ret, _, err := unregisterEventsMapCallback.Call(e.perfBuffer)
+
+	if ret != 0 {
+		return err
+	}
+
+	return nil
+}
