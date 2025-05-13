50541d9b70eac62921a2cbfad82145eff937bae8
Kamil <kamil.prz@gmail.com>
2025-05-13 18:03:08 +0100

    annotation constants

---
+++ pkg/capture/utils/annotations.go
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+package utils
+
+import (
+	retinav1alpha1 "github.com/microsoft/retina/crd/api/v1alpha1"
+	captureConstants "github.com/microsoft/retina/pkg/capture/constants"
+	"github.com/microsoft/retina/pkg/capture/file"
+)
+
+func GetPodAnnotationsFromCapture(capture *retinav1alpha1.Capture) map[string]string {
+	return map[string]string{
+		captureConstants.CaptureFilenameAnnotationKey:  capture.Name,
+		captureConstants.CaptureTimestampAnnotationKey: file.TimeToString(capture.Status.StartTime),
+		captureConstants.CaptureHostPathAnnotaionKey:   *capture.Spec.OutputConfiguration.HostPath,
+	}
+}
