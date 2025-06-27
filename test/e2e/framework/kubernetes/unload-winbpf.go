3530a9d5b6176874553e439dfb9e157fb36e7087
VINOD KLSWAMY <50496432+kumarvin123@users.noreply.github.com>
2025-06-27 11:40:38 -0700

    Update unload-winbpf.go
    Signed-off-by: VINOD KLSWAMY <50496432+kumarvin123@users.noreply.github.com>

--- test/e2e/framework/kubernetes/unload-winbpf.go
+++ test/e2e/framework/kubernetes/unload-winbpf.go
@@ -13,11 +13,9 @@ type UnLoadAndPinWinBPF struct {
 
 func (a *UnLoadAndPinWinBPF) Run() error {
 	UnLoadAndPinWinBPFDLabelSelector := fmt.Sprintf("name=%s", a.UnLoadAndPinWinBPFDeamonSetName)
-	output, err := ExecCommandInWinPod(a.KubeConfigFilePath, "C:\\event-writer-helper.bat EventWriter-UnPinPrgAndMaps", a.UnLoadAndPinWinBPFDeamonSetNamespace, UnLoadAndPinWinBPFDLabelSelector)
-	if err != nil {
-		return err
-	}
+	output, _ := ExecCommandInWinPod(a.KubeConfigFilePath, "C:\\event-writer-helper.bat EventWriter-UnPinPrgAndMaps", a.UnLoadAndPinWinBPFDeamonSetNamespace, UnLoadAndPinWinBPFDLabelSelector)
 
+	// Best effort to wait for the eBPF program unload to complete. We dont fail the test if it fails.
 	fmt.Println(output)
 	if strings.Contains(output, "error") || strings.Contains(output, "failed") {
 		return fmt.Errorf("error in UnLoading and pinning BPF maps and program: %s", output)
