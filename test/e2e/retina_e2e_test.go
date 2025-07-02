0b24729706854e3e82e178c059731ec8ce40c805
VINOD KLSWAMY <50496432+kumarvin123@users.noreply.github.com>
2025-07-02 15:51:42 -0700

    (test): Retina Windows eBPF e2e Tests (#1721)
    Added e2e tests for Windows ebpf scenario.
    Please provide a brief description of the changes made in this pull
    request.
    1. Creates a daemon set to install ebpf on windows nodes
    2. Installs a test ebpf program which generates the events
    3. The generated events are verified by the test jobs
    
    If this pull request is related to any issue, please mention it here.
    Additionally, make sure that the issue is assigned to you before
    submitting this pull request.
    
    - [x] I have read the [contributing
    documentation](https://retina.sh/docs/Contributing/overview).
    - [x] I signed and signed-off the commits (`git commit -S -s ...`). See
    [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [x] I have correctly attributed the author(s) of the code.
    - [x] I have tested the changes locally.
    - [ ] I have followed the project's style guidelines.
    - [ ] I have updated the documentation, if necessary.
    - [ ] I have added tests, if applicable.
    
    successful run with this change
    https://github.com/kumarvin123/retina/actions/runs/15951308276
    
    Please add any relevant screenshots or GIFs to showcase the changes
    made.
    
    Add any additional notes or context about the pull request here.
    
    ---
    
    Please refer to the [CONTRIBUTING.md](../CONTRIBUTING.md) file for more
    information on how to contribute to this project.

--- test/e2e/retina_e2e_test.go
+++ test/e2e/retina_e2e_test.go
@@ -3,12 +3,15 @@
 package retina
 
 import (
+	"context"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/microsoft/retina/test/e2e/common"
 	"github.com/microsoft/retina/test/e2e/framework/helpers"
+	"github.com/microsoft/retina/test/e2e/framework/kubernetes"
 	"github.com/microsoft/retina/test/e2e/framework/types"
 	"github.com/microsoft/retina/test/e2e/infra"
 	jobs "github.com/microsoft/retina/test/e2e/jobs"
@@ -33,7 +36,20 @@ func TestE2ERetina(t *testing.T) {
 		*common.KubeConfig = infra.CreateAzureTempK8sInfra(ctx, t, rootDir)
 	}
 
+	// Install Ebpf and XDP
+	installEbpfAndXDP := types.NewRunner(t, jobs.InstallEbpfXdp(common.KubeConfigFilePath(rootDir)))
+	installEbpfAndXDP.Run(ctx)
+
+	// Wait for the HPC pod to be ready. Maximum wait time is 15 minutes in case the Pods are very slow to come up.
+	err = kubernetes.WaitForPodReadyWithTimeOut(context.TODO(), common.KubeConfigFilePath(rootDir), "install-ebpf-xdp", "name=install-ebpf-xdp", 15*time.Minute)
+	require.NoError(t, err)
+
+	// Load and pin BPF Maps
+	loadAndPinWinBPFJob := types.NewRunner(t, jobs.LoadAndPinWinBPFJob(common.KubeConfigFilePath(rootDir)))
+	loadAndPinWinBPFJob.Run(ctx)
+
 	// Install and test Retina basic metrics
+
 	basicMetricsE2E := types.NewRunner(t,
 		jobs.InstallAndTestRetinaBasicMetrics(
 			common.KubeConfigFilePath(rootDir),
@@ -59,6 +75,13 @@ func TestE2ERetina(t *testing.T) {
 	// Install Retina basic and test captures
 	captureE2E := types.NewRunner(t,
 		jobs.ValidateCapture(
+	// unpin BPF Maps
+	unloadAndPinWinBPFJob := types.NewRunner(t, jobs.UnLoadAndPinWinBPFJob(common.KubeConfigFilePath(rootDir)))
+	unloadAndPinWinBPFJob.Run(ctx)
+
+	// Install and test Hubble basic metrics
+	validatehubble := types.NewRunner(t,
+		jobs.ValidateHubble(
 			common.KubeConfigFilePath(rootDir),
 			"default"),
 	)
