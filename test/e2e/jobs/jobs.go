246994139df526043da4522eba7162515f1e5cb2
VINOD KLSWAMY <50496432+kumarvin123@users.noreply.github.com>
2025-07-02 15:51:42 -0700

    (test): Retina Windows eBPF e2e Tests (#1721)
    Added e2e tests for Windows ebpf scenario.
    Please provide a brief description of the changes made in this pull
    request.
    1. Creates a daemon set to install ebpf on windows nodes
    2. Installs a test ebpf program which generates the events
    3. The generated events are verified by the test jobs
    
    If this pull request is related to any issue, please mention it here.
    Additionally, make sure that the issue is assigned to you before
    submitting this pull request.
    
    - [x] I have read the [contributing
    documentation](https://retina.sh/docs/Contributing/overview).
    - [x] I signed and signed-off the commits (`git commit -S -s ...`). See
    [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [x] I have correctly attributed the author(s) of the code.
    - [x] I have tested the changes locally.
    - [ ] I have followed the project's style guidelines.
    - [ ] I have updated the documentation, if necessary.
    - [ ] I have added tests, if applicable.
    
    successful run with this change
    https://github.com/kumarvin123/retina/actions/runs/15951308276
    
    Please add any relevant screenshots or GIFs to showcase the changes
    made.
    
    Add any additional notes or context about the pull request here.
    
    ---
    
    Please refer to the [CONTRIBUTING.md](../CONTRIBUTING.md) file for more
    information on how to contribute to this project.

--- test/e2e/jobs/jobs.go
+++ test/e2e/jobs/jobs.go
@@ -1,36 +1,22 @@
 package retina
 
 import (
-	"fmt"
-
-	armnetwork "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v5"
 	"github.com/microsoft/retina/test/e2e/common"
 	"github.com/microsoft/retina/test/e2e/framework/azure"
 	"github.com/microsoft/retina/test/e2e/framework/generic"
 	"github.com/microsoft/retina/test/e2e/framework/kubernetes"
 	"github.com/microsoft/retina/test/e2e/framework/types"
-	"github.com/microsoft/retina/test/e2e/scenarios/capture"
+	"github.com/microsoft/retina/test/e2e/hubble"
+
 	"github.com/microsoft/retina/test/e2e/scenarios/dns"
 	"github.com/microsoft/retina/test/e2e/scenarios/drop"
-	hubble_dns "github.com/microsoft/retina/test/e2e/scenarios/hubble/dns"
-	hubble_drop "github.com/microsoft/retina/test/e2e/scenarios/hubble/drop"
-	hubble_flow "github.com/microsoft/retina/test/e2e/scenarios/hubble/flow"
-	hubble_service "github.com/microsoft/retina/test/e2e/scenarios/hubble/service"
-	hubble_tcp "github.com/microsoft/retina/test/e2e/scenarios/hubble/tcp"
 	"github.com/microsoft/retina/test/e2e/scenarios/latency"
 	tcp "github.com/microsoft/retina/test/e2e/scenarios/tcp"
 	"github.com/microsoft/retina/test/e2e/scenarios/windows"
 )
 
-const IPPrefix = "serviceTaggedIp"
-
 func CreateTestInfra(subID, rg, clusterName, location, kubeConfigFilePath string, createInfra bool) *types.Job {
 	job := types.NewJob("Create e2e test infrastructure")
-
-	publicIPID := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/publicIPAddresses", subID, clusterName)
-	publicIPv4FullName := fmt.Sprintf("%s/%s-%s-v4", publicIPID, IPPrefix, clusterName)
-	publicIPv6FullName := fmt.Sprintf("%s/%s-%s-v6", publicIPID, IPPrefix, clusterName)
-
 	if createInfra {
 		job.AddStep(&azure.CreateResourceGroup{
 			SubscriptionID:    subID,
@@ -48,31 +34,11 @@ func CreateTestInfra(subID, rg, clusterName, location, kubeConfigFilePath string
 			SubnetAddressSpace: "10.0.0.0/12",
 		}, nil)
 
-		job.AddStep(&azure.CreatePublicIP{
-			ClusterName: clusterName,
-			IPVersion:   string(armnetwork.IPVersionIPv4),
-			IPPrefix:    IPPrefix,
-		}, &types.StepOptions{
-			SkipSavingParametersToJob: true,
-		})
-
-		job.AddStep(&azure.CreatePublicIP{
-			ClusterName: clusterName,
-			IPVersion:   string(armnetwork.IPVersionIPv6),
-			IPPrefix:    IPPrefix,
-		}, &types.StepOptions{
-			SkipSavingParametersToJob: true,
-		})
-
 		job.AddStep(&azure.CreateNPMCluster{
 			ClusterName:  clusterName,
 			PodCidr:      "10.128.0.0/9",
 			DNSServiceIP: "192.168.0.10",
 			ServiceCidr:  "192.168.0.0/28",
-			PublicIPs: []string{
-				publicIPv4FullName,
-				publicIPv6FullName,
-			},
 		}, nil)
 
 		job.AddStep(&azure.GetAKSKubeConfig{
@@ -130,6 +96,18 @@ func UninstallRetina(kubeConfigFilePath, chartPath string) *types.Job {
 		KubeConfigFilePath: kubeConfigFilePath,
 	}, nil)
 
+	return job
+}
+
+func InstallEbpfXdp(kubeConfigFilePath string) *types.Job {
+	job := types.NewJob("Install EBPF and XDP")
+	job.AddStep(&kubernetes.CreateNamespace{
+		KubeConfigFilePath: kubeConfigFilePath,
+		Namespace:          "install-ebpf-xdp"}, nil)
+
+	job.AddStep(&kubernetes.ApplyYamlConfig{
+		YamlFilePath: "yaml/windows/install-ebpf-xdp.yaml",
+	}, nil)
 	return job
 }
 
@@ -193,9 +171,9 @@ func InstallAndTestRetinaBasicMetrics(kubeConfigFilePath, chartPath string, test
 			name := scenario.name + " - Arch: " + arch
 			job.AddScenario(dns.ValidateBasicDNSMetrics(name, scenario.req, scenario.resp, testPodNamespace, arch))
 		}
-
-		job.AddScenario(windows.ValidateWindowsBasicMetric())
 	}
+
+	job.AddScenario(windows.ValidateWindowsBasicMetric())
 
 	job.AddStep(&kubernetes.EnsureStableComponent{
 		PodNamespace:           common.KubeSystemNamespace,
@@ -208,6 +186,7 @@ func InstallAndTestRetinaBasicMetrics(kubeConfigFilePath, chartPath string, test
 
 func UpgradeAndTestRetinaAdvancedMetrics(kubeConfigFilePath, chartPath, valuesFilePath string, testPodNamespace string) *types.Job {
 	job := types.NewJob("Upgrade and test Retina with advanced metrics")
+
 	// enable advanced metrics
 	job.AddStep(&kubernetes.UpgradeRetinaHelmChart{
 		Namespace:          common.KubeSystemNamespace,
@@ -259,6 +238,11 @@ func UpgradeAndTestRetinaAdvancedMetrics(kubeConfigFilePath, chartPath, valuesFi
 		},
 	}
 
+	// Validate Windows BPF Metrics
+	job.AddStep(&kubernetes.ApplyYamlConfig{
+		YamlFilePath: "yaml/windows/non-hpc-pod.yaml",
+	}, nil)
+
 	for _, arch := range common.Architectures {
 		for _, scenario := range dnsScenarios {
 			name := scenario.name + " - Arch: " + arch
@@ -266,6 +250,7 @@ func UpgradeAndTestRetinaAdvancedMetrics(kubeConfigFilePath, chartPath, valuesFi
 		}
 	}
 
+	job.AddScenario(windows.ValidateWinBpfMetricScenario())
 	job.AddScenario(latency.ValidateLatencyMetric(testPodNamespace))
 
 	job.AddStep(&kubernetes.EnsureStableComponent{
@@ -277,10 +262,10 @@ func UpgradeAndTestRetinaAdvancedMetrics(kubeConfigFilePath, chartPath, valuesFi
 	return job
 }
 
-func InstallAndTestHubbleMetrics(kubeConfigFilePath, chartPath string) *types.Job {
+func ValidateHubble(kubeConfigFilePath, chartPath string, testPodNamespace string) *types.Job {
 	job := types.NewJob("Validate Hubble")
 
-	job.AddStep(&kubernetes.InstallHubbleHelmChart{
+	job.AddStep(&kubernetes.ValidateHubbleStep{
 		Namespace:          common.KubeSystemNamespace,
 		ReleaseName:        "retina",
 		KubeConfigFilePath: kubeConfigFilePath,
@@ -288,41 +273,15 @@ func InstallAndTestHubbleMetrics(kubeConfigFilePath, chartPath string) *types.Jo
 		TagEnv:             generic.DefaultTagEnv,
 	}, nil)
 
-	hubbleScenarios := []*types.Scenario{
-		hubble_service.ValidateHubbleRelayService(),
-		hubble_service.ValidateHubbleUIService(kubeConfigFilePath),
-	}
+	job.AddScenario(hubble.ValidateHubbleRelayService())
 
-	for _, arch := range common.Architectures {
-		hubbleScenarios = append(hubbleScenarios,
-			hubble_dns.ValidateDNSMetric(arch),
-			hubble_flow.ValidatePodToPodIntraNodeHubbleFlowMetric(arch),
-			hubble_flow.ValidatePodToPodInterNodeHubbleFlowMetric(arch),
-			hubble_flow.ValidatePodToWorldHubbleFlowMetric(arch),
-			hubble_drop.ValidateDropMetric(arch),
-			hubble_tcp.ValidateTCPMetric(arch),
-		)
-	}
-
-	for _, scenario := range hubbleScenarios {
-		job.AddScenario(scenario)
-	}
+	job.AddScenario(hubble.ValidateHubbleUIService(kubeConfigFilePath))
 
 	job.AddStep(&kubernetes.EnsureStableComponent{
 		PodNamespace:           common.KubeSystemNamespace,
 		LabelSelector:          "k8s-app=retina",
 		IgnoreContainerRestart: false,
 	}, nil)
-
-	return job
-}
-
-func ValidateCapture(kubeConfigFilePath, testPodNamespace string) *types.Job {
-	job := types.NewJob("Validate Capture")
-
-	job.AddScenario(capture.ValidateCapture(
-		kubeConfigFilePath,
-		testPodNamespace))
 
 	return job
 }
@@ -334,6 +293,28 @@ func LoadGenericFlags() *types.Job {
 		TagEnv:            generic.DefaultTagEnv,
 		ImageNamespaceEnv: generic.DefaultImageNamespace,
 		ImageRegistryEnv:  generic.DefaultImageRegistry,
+	}, nil)
+
+	return job
+}
+
+func LoadAndPinWinBPFJob(kubeConfigFilePath string) *types.Job {
+	job := types.NewJob("Load Windows BPF Maps")
+	job.AddStep(&kubernetes.LoadAndPinWinBPF{
+		KubeConfigFilePath:                 kubeConfigFilePath,
+		LoadAndPinWinBPFDeamonSetNamespace: "install-ebpf-xdp",
+		LoadAndPinWinBPFDeamonSetName:      "install-ebpf-xdp",
+	}, nil)
+
+	return job
+}
+
+func UnLoadAndPinWinBPFJob(kubeConfigFilePath string) *types.Job {
+	job := types.NewJob("Unload Windows BPF Maps")
+	job.AddStep(&kubernetes.UnLoadAndPinWinBPF{
+		KubeConfigFilePath:                   kubeConfigFilePath,
+		UnLoadAndPinWinBPFDeamonSetNamespace: "install-ebpf-xdp",
+		UnLoadAndPinWinBPFDeamonSetName:      "install-ebpf-xdp",
 	}, nil)
 
 	return job
