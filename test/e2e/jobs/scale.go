19f8ec7b2987a6bf93a27dd0b37726a775a7d62b
Keerthana Routhu <krouthu@microsoft.com>
2025-08-04 21:22:12 -0700

    test

--- test/e2e/jobs/scale.go
+++ test/e2e/jobs/scale.go
@@ -14,7 +14,7 @@ import (
 
 func DefaultScaleTestOptions() scaletest.Options {
 	return scaletest.Options{
-		Namespace:                     "scale-test",
+		Namespace:                     "krouthu-scale-test",
 		MaxKwokPodsPerNode:            0,
 		NumKwokDeployments:            0,
 		NumKwokReplicas:               0,
