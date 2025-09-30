4dbd6f054d0d9766439a830fc7b79e864cc326e4
beegiik <kbeegii@gmail.com>
2025-09-30 18:10:32 +0100

    feat(standalone): Polishing

--- pkg/controllers/daemon/standalone/source/types.go
+++ pkg/controllers/daemon/standalone/source/types.go
@@ -5,6 +5,8 @@ package source
 
 import "github.com/microsoft/retina/pkg/common"
 
+//go:generate go run go.uber.org/mock/mockgen@v0.4.0 -destination=mock_source.go  -copyright_file=../lib/ignore_headers.txt -package=source github.com/microsoft/retina/pkg/controllers/daemon/standalone/source Source
+
 type Source interface {
 	// GetAllEndpoints retrieves all retina endpoints from its corresponding source
 	GetAllEndpoints() ([]*common.RetinaEndpoint, error)
