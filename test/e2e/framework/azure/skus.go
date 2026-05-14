40c23d9bde62a16519fac851d5bb2ea966679021
Alex Castilio dos Santos <alexsantos@microsoft.com>
2026-05-14 16:41:15 +0100

    fix: update scale test sku
    Signed-off-by: Alex Castilio dos Santos <alexsantos@microsoft.com>

--- test/e2e/framework/azure/skus.go
+++ test/e2e/framework/azure/skus.go
@@ -3,7 +3,7 @@ package azure
 import "os"
 
 var (
-	AgentLinuxSKU    = skuFromEnv("AZURE_AGENT_LINUX_SKU", "Standard_D4ds_v4")
+	AgentLinuxSKU    = skuFromEnv("AZURE_AGENT_LINUX_SKU", "Standard_D4ds_v3")
 	AgentLinuxARMSKU = skuFromEnv("AZURE_AGENT_LINUX_ARM_SKU", "Standard_D4pds_v5")
 	AgentWindowsSKU  = skuFromEnv("AZURE_AGENT_WINDOWS_SKU", "Standard_D4ds_v4")
 )
