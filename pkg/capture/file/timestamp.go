bff61ebcd5f6959474f5f131ee21987aa5900eab
Laksh <lakshkotian98@gmail.com>
2026-06-09 10:28:34 -0700

    Laksh/cherry pick specific (#2425)
    Problem
    
    The OB pipeline for tag v0.0.33-windows-rc.2 fails with two errors:
    
    1. kubectl_shell: docker build fails because target shell-target not
    found in cli/Dockerfile
     2. agent_win: output/windows_amd64/*.exe not found
    
    Root cause: branch dev/v0.0.33-windows was created from commit 82d621c
    (May 7, 2025) and is missing upstream changes that restructured the
    Windows build pipeline.
    
    Cherry-picked following commits to resolve the issue:
    [59c1655c](https://github.com/microsoft/retina/commit/59c1655c07afe60ccd4d518114d750bc300a3f16)
    (1946) and
    [fd482c08](https://github.com/microsoft/retina/commit/fd482c08818b21b4d5515647382f71085b353528)
    (#1997)
    
    If this pull request is related to any issue, please mention it here.
    Additionally, make sure that the issue is assigned to you before
    submitting this pull request.
    
    - [ ] I have read the [contributing
    documentation](https://retina.sh/docs/Contributing/overview).
    - [ ] I signed and signed-off the commits (`git commit -S -s ...`). See
    [this
    documentation](https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification)
    on signing commits.
    - [ ] I have correctly attributed the author(s) of the code.
    - [ ] I have tested the changes locally.
    - [ ] I have followed the project's style guidelines.
    - [ ] I have updated the documentation, if necessary.
    - [ ] I have added tests, if applicable.
    
    Please add any relevant screenshots or GIFs to showcase the changes
    made.
    
    Add any additional notes or context about the pull request here.
    
    ---
    
    Please refer to the [CONTRIBUTING.md](../CONTRIBUTING.md) file for more
    information on how to contribute to this project.
    
    ---------
    
    Signed-off-by: Kamil <kamil.prz@gmail.com>
    Signed-off-by: Laksh Kotian <lakshkotian98@gmail.com>
    Co-authored-by: carlotaarvela <67275927+carlotaarvela@users.noreply.github.com>
    Co-authored-by: Kamil <kamil.prz@gmail.com>
    Co-authored-by: kamilprz <36544756+kamilprz@users.noreply.github.com>
    Co-authored-by: Simone Rodigari <32323373+SRodi@users.noreply.github.com>

--- pkg/capture/file/timestamp.go
+++ pkg/capture/file/timestamp.go
@@ -31,3 +31,8 @@ func TimeToString(timestamp *metav1.Time) string {
 	}
 	return timestamp.UTC().Format(captureFileNameTimestampFormat)
 }
+
+// TimeToString converts a Timestamp to a string in the capture file name format.
+func TimeToString(t Timestamp) string {
+	return t.Time.UTC().Format(captureFileNameTimestampFormat)
+}
